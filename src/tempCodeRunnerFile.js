de4543e90bed930e509de8a0e9eb79a465f0b704
prizma-ru <dmitry.glotov@gmail.com>
2021-08-29 21:01:26 +0300

    feat: start task tic-tac-toe

---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+this.matrix[rowIndex][columnIndex]
--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,25 +1,66 @@
 class TicTacToe {
     constructor() {
+        this.currentPlayer = "x";
+        this.matrix = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ];
+        this.turns = 0;
 
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.currentPlayer;
     }
 
     nextTurn(rowIndex, columnIndex) {
+        if (!this.matrix[rowIndex][columnIndex]) {
+            this.matrix[rowIndex][columnIndex] = this.currentPlayer;
+            this.currentPlayer = this.currentPlayer === "x" ? "o" : "x"; //надо присвоить по условию
+            this.turns++;
+            console.log(this.matrix);
+        }
 
     }
 
     isFinished() {
 
+
     }
 
     getWinner() {
-
+        if (
+            (this.matrix[0][0] === 'x' && this.matrix[0][1] === 'x' && this.matrix[0][2] === 'x') ||
+            (this.matrix[1][0] === 'x' && this.matrix[1][1] === 'x' && this.matrix[1][2] === 'x') ||
+            (this.matrix[2][0] === 'x' && this.matrix[2][1] === 'x' && this.matrix[2][2] === 'x') ||
+            //-----------------
+            (this.matrix[0][0] === 'x' && this.matrix[1][0] === 'x' && this.matrix[2][0] === 'x') ||
+            (this.matrix[0][1] === 'x' && this.matrix[1][1] === 'x' && this.matrix[2][1] === 'x') ||
+            (this.matrix[0][2] === 'x' && this.matrix[1][2] === 'x' && this.matrix[2][2] === 'x') ||
+            //-----------------
+            (this.matrix[0][0] === 'x' && this.matrix[1][1] === 'x' && this.matrix[2][2] === 'x') ||
+            (this.matrix[2][0] === 'x' && this.matrix[1][1] === 'x' && this.matrix[0][2] === 'x')) {
+            return "x";
+        }
+        else if (
+            (this.matrix[0][0] === 'o' && this.matrix[0][1] === 'o' && this.matrix[0][2] === 'o') ||
+            (this.matrix[1][0] === 'o' && this.matrix[1][1] === 'o' && this.matrix[1][2] === 'o') ||
+            (this.matrix[2][0] === 'o' && this.matrix[2][1] === 'o' && this.matrix[2][2] === 'o') ||
+            //-----------------
+            (this.matrix[0][0] === 'o' && this.matrix[1][0] === 'o' && this.matrix[2][0] === 'o') ||
+            (this.matrix[0][1] === 'o' && this.matrix[1][1] === 'o' && this.matrix[2][1] === 'o') ||
+            (this.matrix[0][2] === 'o' && this.matrix[1][2] === 'o' && this.matrix[2][2] === 'o') ||
+            //-----------------
+            (this.matrix[0][0] === 'o' && this.matrix[1][1] === 'o' && this.matrix[2][2] === 'o') ||
+            (this.matrix[2][0] === 'o' && this.matrix[1][1] === 'o' && this.matrix[0][2] === 'o')) {
+            return "o";
+        }
+        return null;
     }
 
     noMoreTurns() {
+        this.turns === 9 ? true : false;
 
     }
 
@@ -28,7 +69,7 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.matrix[rowIndex][colIndex];
     }
 }
 
